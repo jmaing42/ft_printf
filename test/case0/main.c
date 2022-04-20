305ce65baffa4c38df23f26c0239061cc9aedec0
Juyeong Maing <mjy9088@naver.com>
2022-04-20 17:59:33 +0000

    norm

--- test/case0/main.c
+++ test/case0/main.c
@@ -1,9 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 11:53:00 by jmaing            #+#    #+#             */
+/*   Updated: 2022/04/20 17:59:23 by jmaing           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+
 #include "../../src/format.h"
 
-int main() {
-	t_printf_format *format = printf_format_parse("Hello %s!");
-	printf("%p\n", format);
-	printf_format_free(format);
-	return 0;
+int	main(void)
+{
+	t_printf_format	*format;
+
+	format = printf_format_parse("Hello %s!");
+	if (format)
+		printf_format_free(format);
+	return (0);
 }
