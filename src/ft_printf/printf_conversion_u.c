f8ea193c00c745ec8b345f7ce13edd47614aa8f5
Juyeong Maing <mjy9088@naver.com>
2022-04-24 14:18:06 +0000

    fix bug, add u

--- src/ft_printf/printf_conversion_u.c
+++ src/ft_printf/printf_conversion_u.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf_conversion_d.c                              :+:      :+:    :+:   */
+/*   printf_conversion_u.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/20 21:27:56 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/24 14:15:46 by jmaing           ###   ########.fr       */
+/*   Created: 2022/04/24 14:05:31 by jmaing            #+#    #+#             */
+/*   Updated: 2022/04/24 14:17:53 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf_conversion_d.h"
+#include "printf_conversion_u.h"
 
 static t_err	print_nonzero(
 	t_ft_vprintf_stream_context *context,
-	t_d *layout,
+	t_u *layout,
 	int minimum_field_width,
 	int precision
 )
@@ -31,9 +31,6 @@ static t_err	print_nonzero(
 	return ((!layout->left
 			&& space_padding
 			&& ft_vprintf_stream_util_print_n(context, space_padding, ' '))
-		|| (layout->sign
-			&& context->stream_class->writer(
-				context->stream, &layout->sign, 1))
 		|| (zero_padding
 			&& ft_vprintf_stream_util_print_n(context, zero_padding, '0'))
 		|| ft_vprintf_stream_i_put(context, layout->value, 10, "0123456789")
@@ -54,49 +51,33 @@ static t_err	print_zero(
 	if (precision == -1)
 		precision = 1;
 	remain = minimum_field_width - precision;
-	if (conversion->flag_always_show_sign
-		|| conversion->flag_use_sign_placeholder)
-		remain--;
 	return ((!conversion->flag_left_justified
 			&& remain > 0
 			&& ft_vprintf_stream_util_print_n(context, remain, ' '))
-		|| (conversion->flag_always_show_sign
-			&& context->stream_class->writer(
-				context->stream, "+", 1))
-		|| (conversion->flag_use_sign_placeholder
-			&& context->stream_class->writer(
-				context->stream, " ", 1))
 		|| ft_vprintf_stream_util_print_n(context, precision, '0')
 		|| (conversion->flag_left_justified
 			&& remain > 0
 			&& ft_vprintf_stream_util_print_n(context, remain, ' ')));
 }
 
-t_err	ft_vprintf_stream_d(
+t_err	ft_vprintf_stream_u(
 	t_ft_vprintf_stream_context *context,
 	va_list arguments,
 	t_printf_format_conversion_specification *conversion
 )
 {
-	t_d			d;
-	const int	mfw = ft_vprintf_get_mfw_actual(arguments, conversion, &d.left);
+	t_u			u;
+	const int	mfw = ft_vprintf_get_mfw_actual(arguments, conversion, &u.left);
 	const int	precision = ft_vprintf_get_precision(arguments, conversion, -1);
 
-	if (ft_vprintf_get_any_d(arguments, conversion->length_modifier, &d.value))
+	if (ft_vprintf_get_any_u(arguments, conversion->length_modifier, &u.value))
 		return (true);
-	if (conversion->flag_use_alternative_form)
+	if (conversion->flag_use_alternative_form
+		|| conversion->flag_always_show_sign
+		|| conversion->flag_use_sign_placeholder)
 		ft_vprintf_stream_undefined_behavior_hooray();
-	if (!d.value)
+	if (!u.value)
 		return (print_zero(context, conversion, mfw, precision));
-	d.length = ft_vprintf_stream_i_length(d.value, 10) + 1;
-	d.sign = 0;
-	if (d.value < 0)
-		d.sign = '-';
-	else if (conversion->flag_always_show_sign)
-		d.sign = '+';
-	else if (conversion->flag_use_sign_placeholder)
-		d.sign = ' ';
-	else
-		d.length--;
-	return (print_nonzero(context, &d, mfw, precision));
+	u.length = ft_vprintf_stream_i_length(u.value, 10);
+	return (print_nonzero(context, &u, mfw, precision));
 }
