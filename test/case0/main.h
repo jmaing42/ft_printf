1f5430481398b441e05f02682574bea0ae35a0a8
맹주영 <mjy9088@naver.com>
2022-04-25 22:08:50 +0900

    separate test

--- test/case0/main.h
+++ test/case0/main.h
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/19 11:53:00 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/25 22:05:03 by jmaing           ###   ########.fr       */
+/*   Created: 2022/04/25 21:59:25 by jmaing            #+#    #+#             */
+/*   Updated: 2022/04/25 22:04:42 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#ifndef MAIN_H
+# define MAIN_H
 
-#include "main.h"
+# include "../../src/libftprintf.h"
 
-int	test(t_printf f)
-{
-	return (test_d(f));
-}
+typedef int	(*t_printf)(const char *format, ...);
 
-int	main(int argc, char **argv)
-{
-	(void) argv;
-	if (argc < 2)
-		return (test(&ft_printf));
-	return (test(&printf));
-}
+int	test_d(t_printf f);
+
+#endif
