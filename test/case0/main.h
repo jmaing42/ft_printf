30d4e9e4b57e6b5a42de743c65885b1385930851
Juyeong Maing <mjy9088@naver.com>
2022-04-26 20:58:33 +0000

    add u test, fix o test

--- test/case0/main.h
+++ test/case0/main.h
@@ -6,7 +6,7 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 21:59:25 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/26 20:52:17 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/04/26 20:53:54 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ typedef int	(*t_printf)(const char *format, ...);
 void	test_d(t_printf f);
 void	test_i(t_printf f);
 void	test_o(t_printf f);
+void	test_u(t_printf f);
 
 #endif
--- test/case0/test_u.c
+++ test/case0/test_u.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_o.c                                           :+:      :+:    :+:   */
+/*   test_u.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 22:02:59 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/26 20:57:52 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/04/26 20:57:59 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,21 +41,21 @@ static const char	*precision[] = {
 	".100"
 };
 
-void	test_o(t_printf f)
+void	test_u(t_printf f)
 {
-	char	fmt[16];
+	char	fmt[15];
 	int		i;
 
 	i = 0;
-	while (i < (1 << 6) * 11)
+	while (i < (1 << 5) * 11)
 	{
-		sprintf(fmt, "\"%%%s%s%s%s%so\"",
-			i & 1 ? "-" : "", i & 2 ? "0" : "", i & 4 ? "#" : "",
-			minimum_field_width[i >> 6], precision[(i >> 3) % 8]);
-		f("Test for %%o - case #%d: %s {\n\tzero: ", i, fmt);
+		sprintf(fmt, "\"%%%s%s%s%su\"",
+			i & 1 ? "-" : "", i & 2 ? "0" : "",
+			minimum_field_width[i >> 5], precision[(i >> 2) % 8]);
+		f("Test for %%u - case #%d: %s {\n\tzero: ", i, fmt);
 		f(fmt, 0);
-		f(",\n\t9054370: ");
-		f(fmt, 042424242);
+		f(",\n\tpositive: ");
+		f(fmt, 42424242);
 		f(",\n\tmax: ");
 		f(fmt, UINT_MAX);
 		f(",\n} end case #%d (%s)\n\n", i, fmt);
