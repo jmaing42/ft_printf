bfe1486e4b9aebd1087d4778e31a8fcdc16d8eeb
Juyeong Maing <mjy9088@naver.com>
2022-04-26 21:03:18 +0000

    remove unused include

--- src/libftprintf.h
+++ src/libftprintf.h
@@ -6,14 +6,12 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 13:15:18 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/22 06:18:35 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/04/26 20:59:47 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
-
-# include "libft/ft_types.h"
 
 /**
  * @brief same as printf
