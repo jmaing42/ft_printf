aed58cff5acd10a49b9359705efba25b94c67465
맹주영 <mjy9088@naver.com>
2022-04-26 22:12:10 +0900

    rename variables

--- src/ft_printf/printf_conversion_d.c
+++ src/ft_printf/printf_conversion_d.c
@@ -6,7 +6,7 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 21:27:56 by jmaing            #+#    #+#             */
-/*   Updated: 2022/04/26 22:02:41 by jmaing           ###   ########.fr       */
+/*   Updated: 2022/04/26 22:11:44 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 #include "../libft/ft_math.h"
 
-static t_err	ft_vprintf_stream_d_zero_fill(
+static t_err	zero_fill(
 	t_ft_vprintf_stream_context *context,
 	t_d *d
 )
@@ -31,7 +31,7 @@ static t_err	ft_vprintf_stream_d_zero_fill(
 	);
 }
 
-static t_err	ft_vprintf_stream_d_with_precision(
+static t_err	with_precision(
 	t_ft_vprintf_stream_context *context,
 	t_d *d
 )
@@ -54,7 +54,7 @@ static t_err	ft_vprintf_stream_d_with_precision(
 			&& ft_vprintf_stream_util_print_n(context, spaces, ' ')));
 }
 
-static t_err	ft_vprintf_stream_d_without_precision(
+static t_err	without_precision(
 	t_ft_vprintf_stream_context *context,
 	t_d *d
 )
@@ -76,33 +76,33 @@ static t_err	ft_vprintf_stream_d_without_precision(
 
 t_err	ft_vprintf_stream_d(
 	t_ft_vprintf_stream_context *context,
-	va_list a,
-	t_printf_format_conversion_specification *c
+	va_list arguments,
+	t_printf_format_conversion_specification *conversion
 )
 {
 	t_d			d;
 	const int	minimum_field_width = ft_vprintf_get_mfw_actual(
-			a, c, &d.left_justified);
-	const int	precision = ft_vprintf_get_precision(a, c, -1);
+			arguments, conversion, &d.left_justified);
+	const int	precision = ft_vprintf_get_precision(arguments, conversion, -1);
 
-	if (ft_vprintf_get_any_d(a, c->length_modifier, &d.value))
+	if (ft_vprintf_get_any_d(arguments, conversion->length_modifier, &d.value))
 		return (true);
-	if (c->flag_use_alternative_form)
+	if (conversion->flag_use_alternative_form)
 		ft_vprintf_stream_undefined_behavior_hooray();
 	d.minimum_field_width = minimum_field_width;
 	d.precision = precision;
 	d.sign = 0;
 	if (d.value < 0)
 		d.sign = '-';
-	else if (c->flag_always_show_sign)
+	else if (conversion->flag_always_show_sign)
 		d.sign = '+';
-	else if (c->flag_use_sign_placeholder)
+	else if (conversion->flag_use_sign_placeholder)
 		d.sign = ' ';
 	if (precision == -1
-		&& !c->flag_left_justified
-		&& c->flag_pad_field_with_zero)
-		return (ft_vprintf_stream_d_zero_fill(context, &d));
+		&& !conversion->flag_left_justified
+		&& conversion->flag_pad_field_with_zero)
+		return (zero_fill(context, &d));
 	if (precision == -1)
-		return (ft_vprintf_stream_d_without_precision(context, &d));
-	return (ft_vprintf_stream_d_with_precision(context, &d));
+		return (without_precision(context, &d));
+	return (with_precision(context, &d));
 }
