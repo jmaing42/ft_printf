e6e02d51e111299a88ae4dc50eeaa4d3165daf2b
맹주영 <mjy9088@naver.com>
2022-07-26 09:17:12 +0900

    add fto_stream_string prototype

---
+++ src/fto/stream/string/fto_stream_string.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fto_stream_string.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 08:52:10 by Juyeong Maing     #+#    #+#             */
+/*   Updated: 2022/07/26 09:06:30 by Juyeong Maing    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FTO_STREAM_STRING_H
+# define FTO_STREAM_STRING_H
+
+# include "fto_stream_string_in.h"
+# include "fto_stream_string_out.h"
+
+#endif
