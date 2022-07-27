467806776c969302e1cf0dfe322dc9183a21c1c9
맹주영 <mjy9088@naver.com>
2022-07-27 23:24:02 +0900

    fix compilation error, add fto_stream_buffered_in

--- src/fto/stream/buffered/in/fto_stream_buffered_in_new.c
+++ src/fto/stream/buffered/in/fto_stream_buffered_in_new.c
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fto_stream_buffered_out_new.c                      :+:      :+:    :+:   */
+/*   fto_stream_buffered_in_new.c                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/26 23:51:16 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/07/27 23:23:38 by Juyeong Maing    ###   ########.fr       */
+/*   Created: 2022/07/27 23:22:02 by Juyeong Maing     #+#    #+#             */
+/*   Updated: 2022/07/27 23:23:45 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fto_stream_buffered_out_v.h"
+#include "fto_stream_buffered_in_v.h"
 
 #include <stdlib.h>
 
-#include "fto_stream_out_v.h"
+#include "fto_stream_in_v.h"
 
-static const struct s_fto_stream_buffered_out_vtable	g_v = {
-	&fto_stream_buffered_out_v_write,
-	&fto_stream_buffered_out_v_flush,
-	(t_fto_stream_buffered_out_v_try_free)(&fto_stream_out_v_try_free),
-	&fto_stream_buffered_out_v_unsafe_free
+static const struct s_fto_stream_buffered_in_vtable	g_v = {
+	&fto_stream_buffered_in_v_read_all,
+	&fto_stream_buffered_in_v_read_all,
+	&fto_stream_buffered_in_v_end,
+	&fto_stream_buffered_in_v_free
 };
 
-t_fto_stream_buffered_out	*new_fto_stream_buffered_out(
-	t_fto_stream_out *stream,
+t_fto_stream_buffered_in	*new_fto_stream_buffered_in(
+	t_fto_stream_in *stream,
 	bool stream_owned,
 	size_t buffer_capacity
 )
 {
-	t_fto_stream_buffered_out *const	result
-		= malloc(sizeof(t_fto_stream_buffered_out) + buffer_capacity);
+	t_fto_stream_buffered_in *const	result
+		= malloc(sizeof(t_fto_stream_buffered_in) + buffer_capacity);
 
 	if (!result)
 	{
 		if (stream_owned)
-			stream->v->unsafe_free(stream);
+			stream->v->free(stream);
 		return (NULL);
 	}
 	result->v.v = &g_v;
@@ -43,5 +43,6 @@ t_fto_stream_buffered_out	*new_fto_stream_buffered_out(
 	result->stream_owned = stream_owned;
 	result->buffer_capacity = buffer_capacity;
 	result->buffer_size = 0;
+	result->buffer_offset = 0;
 	return (result);
 }
